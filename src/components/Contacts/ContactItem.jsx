fe4d147799b6250481dd094b201f8d82e9876195
choochoocho <choochocho@outlook.com>
2017-09-10 22:03:34 +0300

    fix link style

--- src/components/Contacts/ContactItem.jsx
+++ src/components/Contacts/ContactItem.jsx
@@ -4,7 +4,11 @@ import styles from './ContactItem.css'
 
 export const ContactItem = (props) => (
     <div className={styles.item}>
-        <a href={props.link} target={props.target}>
+        <a
+            href={props.link}
+            target={props.target}
+            className={styles.link}
+        >
             <div className={styles.icon}>
                 <Icon name={props.icon}/>
             </div>
